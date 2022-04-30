c24b2ef0ae2f7fd17429cabb8bf2bd7084c2a4ee
mrkasri <mohammed.kasri1@gmail.com>
2022-04-30 23:55:28 +0000

    draw ellipse

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from distutils.command.build import build
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.graphics import Ellipse
+from kivy.graphics import Color
+
+Window.clearcolor =(1,1,1,1)
 
 class PaintWindow(Widget):
-    pass
+    def on_touch_down(self, touch):
+        self.canvas.add(Color(rgb=(1,0,0)))
+        d=30
+        self.canvas.add(Ellipse(pos=(touch.x-d/2,touch.y-d/2), size=(d,d)))
 
 class PaintApp(App):
 
@@ -12,4 +19,4 @@ class PaintApp(App):
         return PaintWindow()
 
 
-PaintApp().run()s
+PaintApp().run()
